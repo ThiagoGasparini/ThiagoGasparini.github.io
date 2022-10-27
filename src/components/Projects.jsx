c4641f32dbe80fbed23500453563661054b3eeb9
Thiago Gasparini <thiago_engprod@outlook.com>
2022-10-27 00:59:26 -0300

    adiciona links dinamicos para github

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import Header from './Header';
 import results from '../utils/data';
 import './css-components/Projects.css';
 
 const Projects = () => {
   return (
     <div>
+      <Header />
       <h1 className="title">Projetos</h1>
       <div className="cards">
         {results.map((result) => (
           <div>
             <p>{result.name}</p>
-            <img className="img-projects" src={result.src} alt="" />
+            <a target="_blank" href={result.github} rel="noreferrer">
+              <img className="img-projects" src={result.src} alt="" />
+            </a>
           </div>
         ))}
       </div>
