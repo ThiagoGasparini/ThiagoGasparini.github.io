d17ca5eb3c2c90a4db3f2f433e3d9ddd6ca4e06c
Thiago Gasparini <thiago_engprod@outlook.com>
2022-10-28 15:06:16 -0300

    portifolio react.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <img id="arrow" className="imgArrow" src={imgArrow} alt="" />
       <div className="name">
         <strong><em><div>Thiago Gasparini</div></em></strong>
-        <em><div>Full Stack Web Developer</div></em>
+        <em><div>Desenvolvedor Web</div></em>
       </div>
     </div>
   );
