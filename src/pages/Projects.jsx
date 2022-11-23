58e42580221c43903c8533e2407f510f314e98b8
Thiago Gasparini <thiago_engprod@outlook.com>
2022-11-23 12:44:46 -0300

    add botões para filtrar projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import results from '../utils/data';
 import './pages-css/Projects.css';
 
 const Projects = () => {
+  const [frontend, setFrontend] = useState([]);
+  const [backend, setBackend] = useState([]);
+  const [fullstack, setFullstack] = useState([]);
+ 
+  const allBtn = () => {
+    setFrontend([]);
+    setBackend([]);
+    setFullstack([]);
+  };
+
+  const frontBtn = () => {
+    const frontendBtn = results.filter((result) => result.type === 'frontend');
+    setFrontend(frontendBtn);
+  };
+
+  const backBtn = () => {
+    const backendBtn = results.filter((result) => result.type === 'backend');
+    setBackend(backendBtn);
+  };
+
+  const fullBtn = () => {
+    const fullstackBtn = results.filter(
+      (result) => result.type === 'fullstack'
+    );
+    setFullstack(fullstackBtn);
+  };
+
+  const maps = (array) =>
+    array.map((result) => (
+      <a target="_blank" href={result.github} rel="noreferrer">
+        <div className="card">
+          <p>{result.name}</p>
+          <p>Tecnologia: {result.description}</p>
+          <img className="img-projects" src={result.src} alt="" />
+        </div>
+      </a>
+    ));
+
   return (
     <div>
       <Header />
       <h1 className="title">Projetos</h1>
+      <div className='btns'>
+        <button className='btn' onClick={allBtn}>Todos</button>
+        <button className='btn' onClick={frontBtn}>Frontend</button>
+        <button className='btn' onClick={backBtn}>Backend</button>
+        <button className='btn' onClick={fullBtn}>Fullstack</button>
+      </div>
       <span className="container">
-        {results.map((result) => (
-          <a target="_blank" href={result.github} rel="noreferrer">
-            <div className="card">
-              <p>{result.name}</p>
-              <p>Tecnologia: {result.description}</p>
-              <img className="img-projects" src={result.src} alt="" />
-            </div>
-          </a>
-        ))}
+        {fullstack.length
+          ? maps(fullstack)
+          : backend.length
+          ? maps(backend)
+          : frontend.length
+          ? maps(frontend)
+          : maps(results)}
       </span>
       <Footer />
     </div>
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,5 +1,5 @@
 import blogImg from '../Images/blogs-api.png';
-import javascriptImg from '../Images/javascript.png'
+import javascriptImg from '../Images/javascript.png';
 import starImg from '../Images/star-wars.jpeg';
 import storeImg from '../Images/store-manager.png';
 import rtlImg from '../Images/tests-with-rtl.png';
@@ -21,98 +21,112 @@ const results = [
     name: 'Cadastro de Consulta - Aplicação FullStack',
     description: 'React.js / Node.js / MySQL',
     github: 'https://github.com/ThiagoGasparini/Projeto-Cadastro-Consultas',
+    type: 'fullstack',
   },
   {
     src: blog,
     name: 'Blog - Aplicação FullStack',
     description: 'EJS / Node.js / MySQL',
     github: 'https://github.com/ThiagoGasparini/Blog-Full-Stack-Aplication-EJS-Node',
+    type: 'fullstack',
   },
   {
     src: blogImg,
     name: 'Blogs API',
     description: 'Node.js / Sequelize',
     github: 'https://github.com/ThiagoGasparini/Blogs-API-Node.js-Sequelize',
+    type: 'backend',
   },
   {
     src: storeImg,
     name: 'Store Manager',
     description: 'Node.js / MSC',
-    github:
-      'https://github.com/ThiagoGasparini/Project-MSC-NodeJS-Store-Manager',
+    github:'https://github.com/ThiagoGasparini/Project-MSC-NodeJS-Store-Manager',
+    type: 'backend',
   },
   {
     src: tmImg,
     name: 'Talker Manager',
     description: 'Node.js',
     github: 'https://github.com/ThiagoGasparini/Project-Node-Js-Talker-Manager',
+    type: 'backend',
   },
   {
     src: tfcImg,
     name: 'TFC - Trybe Futebol Clube',
     description: 'TypeScript / POO',
-    github:
-      'https://github.com/ThiagoGasparini/Trybe-Futebol-Clube-TypeScript-POO',
+    github: 'https://github.com/ThiagoGasparini/Trybe-Futebol-Clube-TypeScript-POO',
+    type: 'backend',
   },
   {
     src: tadImg,
     name: 'Trybers and Dragons',
     description: 'POO',
     github: 'https://github.com/ThiagoGasparini/Project-Tryber-And-Dragon-POO',
+    type: 'backend',
   },
   {
     src: tsImg,
     name: 'TrybeSmith',
     description: 'TypeScript',
     github: 'https://github.com/ThiagoGasparini/Project-TypeScript-TrybeSmith',
+    type: 'backend',
   },
   {
     src: dockerImg,
     name: 'Docker to do List',
     description: 'Docker',
     github: 'https://github.com/ThiagoGasparini/docker-todo-list',
+    type: 'fullstack',
   },
   {
     src: mysqlImg,
     name: 'One For All',
     description: 'MySQL',
     github: 'https://github.com/ThiagoGasparini/MySQL-Project-One-For-All',
+    type: 'backend',
   },
   {
     src: mongoDbImg,
     name: 'Commerce',
     description: 'MongoDB',
     github: 'https://github.com/ThiagoGasparini/Project-Commerce-MongoDB',
+    type: 'backend',
   },
   {
     src: javascriptImg,
     name: 'JS Functions',
     description: 'JavaScript',
     github: 'https://github.com/ThiagoGasparini/JavaScript-Functions',
+    type: 'frontend',
   },
   {
     src: todolistImg,
     name: 'to-do-list',
     description: 'JavaScript',
     github: 'https://github.com/ThiagoGasparini/To-do-List',
+    type: 'frontend',
   },
   {
     src: jestImg,
     name: 'JS Testes Unitários',
     description: 'Testes Unitários / JavaScript',
     github: 'https://github.com/ThiagoGasparini/project-js-unit-tests',
+    type: 'frontend',
   },
   {
     src: starImg,
     name: 'Star Wars',
     description: 'React / Hooks',
     github: 'https://github.com/ThiagoGasparini/project-starwars-react-hooks',
+    type: 'frontend',
   },
   {
     src: rtlImg,
     name: 'RTL - React Testing Library',
     description: 'React Testing Library',
     github: 'https://github.com/ThiagoGasparini/project-react-testing-library',
+    type: 'frontend',
   },
 ];
 
