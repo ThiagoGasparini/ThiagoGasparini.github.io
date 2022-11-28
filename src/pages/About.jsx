3eef1fb7ef8bb5f9d588123014aa0a09933a7343
Thiago Gasparini <thiago_engprod@outlook.com>
2022-11-28 11:08:14 -0300

    add alterações no sobre

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,30 +13,33 @@ const About = () => {
         <div className="aboutMe">
           <h1 className="title">Olá,</h1>
           <p>
-            Me chamo Thiago Gasparini, tenho 29 anos de idade e moro em
-            Curitiba-PR
+            Me chamo Thiago Gasparini, tenho 29 anos e sou graduado em
+            Engenharia Civil.
           </p>
           <p>
-            Graduado pelo Centro Universitário UniOpet em Engenharia Civil e
-            atualmente estudante de Desenvolvimento Web Full Stack na Trybe.
+            Por não ter me identificado com as áreas da minha graduação e com
+            grande desejo de migrar para o desenvolvimento de software, que
+            reverberou internamente por muito tempo em minha vida, decidi então
+            iniciar minha transição de carreira para o mundo da programação em
+            dezembro de 2021.
           </p>
           <p>
-            Com grande desejo de migrar para a área de desenvolvimento de
-            software, que reverberou internamente por muito tempo em minha vida,
-            decidi então iniciar minha transição de
-            carreira em dezembro de 2021.
+            Atualmente sou estudante de desenvolvimento Web Full Stack na Trybe,
+            faço alguns cursos profissionalizantes pela Udemy e já realizei
+            alguns projetos, como consta em meu portfólio, linkedin e github.
           </p>
           <p>
-            Maravilhado pela conexão do front-end com o back-end, resolvi me
-            desenvolver como Full Stack. Possuo conhecimentos em HTML5, CSS3,
-            JavaScript, React.js, Testes Automatizados com Jest e RTL no front.
-            MySQL, Docker, Node.js, TypeScript, POO, MongoDB e testes com Mocha
-            e Sinon no back. Também estou estudando e me aprimorando com cursos
-            pela Udemy.
+            Maravilhado pela conexão do Front-end com o Back-end, resolvi me
+            aprofundar na carreira como Full Stack. Possuo conhecimentos em
+            HTML5, CSS3, JavaScript, React.js, EJS, Testes unitários com Jest e
+            RTL no Front-end. MySQL, PostgreSQL, Docker, Node.js, Express.js,
+            Sequelize, TypeORM, TypeScript, POO, MongoDB e testes com Mocha,
+            Chai e Sinon no Back-end. Assim como metodologias ágeis (SCRUM e
+            KANBAN).
           </p>
           <p>
             Sou apaixonado por tecnologia e como ela pode impactar e melhorar a
-            nossa qualidade de vida.
+            qualidade de vida das pessoas.
           </p>
         </div>
       </div>
